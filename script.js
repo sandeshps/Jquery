b2974dfd1a599da4eccf0307523df99dee4cc7ad
sandesh <sandeshps1989@yahoo.com>
2013-06-11 14:17:30 +0530

    In progress

--- script.js
+++ script.js
@@ -5,52 +5,48 @@
  * Time: 11:34 AM
  * To change this template use File | Settings | File Templates.
  */
-
+ 
 
 
 $(document).ready(function () {
+    $("#btn").click(function () {
+                    var l= $("#txtInput").val().length;
+                    var st = $("#txtInput").val();
+                    for( var i=0;i<l;i++)
+                        $("#test").append(st[i]);
+                    });
     $("#txtInput").keyup(function () {
-        $(".display-box").html($("#txtInput").val());     
         var sum=0;
-        var initial_count=0;
-        var temp_character;
-        for(var character in $("#txtInput").val())
+        var text_length= $("#txtInput").val().length;
+        var text = $("#txtInput").val();
+        $(".display-box").html($("#txtInput").val());
+        for(var i=0;i<text_length;i++)
         {
-            temp_character=character;
-            for(var temp_char in $("#txtInput").val())
+            for( var j=i+1;j<text_length;j++)
             {
-                if(initial_count==0)
-                {
-                    initial_count++;
+                if(text[i]==text[j])
                     continue;
-                }
                 else
                 {
-                    if(temp_character==temp_char)
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        if( temp_character=='A' || temp_character=='I' || temp_character=='J' || temp_character=='Q' || temp_character=='Y')
+                    if( text[i]=='A' || text[i]=='I' || text[i]=='J' || text[i]=='Q' || text[i]=='Y')
                             sum=sum+1;
-                        else if(temp_character=='B' || temp_character=='K' || temp_character=='R')
+                        else if(text[i]=='B' || text[i]=='K' || text[i]=='R')
                             sum=sum+2;
-                            else if(temp_character=='C' || temp_character=='G' || temp_character=='L' || temp_character=='S')
+                            else if(text[i]=='C' || text[i]=='G' || text[i]=='L' || text[i]=='S')
                                 sum=sum+3;
-                            else if(temp_character=='D' || temp_character=='M' || temp_character=='T')
+                            else if(text[i]=='D' || text[i]=='M' || text[i]=='T')
                                 sum=sum+4;
-                                else if(temp_character=='F' || temp_character=='P')
+                                else if(text[i]=='F' || text[i]=='P')
                                     sum=sum+8;
-                                else if(temp_character=='E' || temp_character=='H' || temp_character=='N' || temp_character=='X')
+                                else if(text[i]=='E' || text[i]=='H' || text[i]=='N' || text[i]=='X')
                                     sum=sum+5;
-                                    else if(temp_character=='U' || temp_character=='V' || temp_character=='W')
+                                    else if(text[i]=='U' || text[i]=='V' || text[i]=='W')
                                         sum=sum+6;
                                     else
-                                        sum=sum+7;
-                    }
+                                        sum=sum+7;   
                 }
             }
+                //$("#test").append(st[i]);
         }
         $("#output").html(sum);
     });   
